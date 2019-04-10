91dba324e0e491cd78166931a36173952cd0e1f9
Septimiu Chiorean <septimiu.chiorean@accesa.eu>
2019-04-10 10:21:03 +0300

    disable source map in production

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
     }),
     new CompressionPlugin()
   ],
-  devtool: 'source-map',
+  devtool: process.env.NODE_ENV === 'production' ? 'none' : 'source-map',
   devServer: {
     host: 'localhost',
     port: 3000,
